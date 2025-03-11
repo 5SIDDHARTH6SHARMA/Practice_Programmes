5e5006b498a941d437a02e21dcb6f63332836952
Siddharth Sharma <5siddharth6sharma@gmail.com>
2025-03-11 13:24:39 +0530

    These are just basic DSA programs

---
+++ PracticeProgrammes/DSA_Practice/PowerOfTwo.java
@@ -0,0 +1,24 @@
+package DSA_Practice;
+
+public class PowerOfTwo {
+
+    public boolean isPowerOfTwo(int number) {
+        boolean bool = false;
+        // My version:
+        // int count = 0;
+        // if(number > 1)
+        // while(number != 0){
+        // count = count + (number & 1);
+        // number = number >> 1;
+        // }
+
+        // if(count == 1)
+        // bool = true;
+
+        // Enhanced version:
+        if (number > 1)
+            return (number & (number - 1)) == 0;
+
+        return bool;
+    }
+}
